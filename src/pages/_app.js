061f6b26be81b225321527f3ced381d6522069ea
joaoricardoM <joao_pia_mds@hotmail.com>
2023-09-06 15:08:18 -0300

    feat: updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,7 @@ import Head from 'next/head'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
-
-const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-mont' })
+import Animated from '@/components/Animated'
 
 export default function Home({}) {
   return (
@@ -15,15 +14,20 @@ export default function Home({}) {
         <meta name="viewport" content="width-device-widt, initial-scale-1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <NavBar />
+
       <main className="flex items-center text-dark w-full min-h-screen">
-        <Layout>
+        <Layout className="pt-0">
           <div className="flex item-center justify-between w-full">
             <div className="w-1/2">
               <Image src={profilePic} alt="Code" className="w-full h-auto" />
             </div>
-            <div>
-              <h1>Changing the world through technology</h1>
+            <div className="w-1/2 flex flex-col items-center self-center ">
+              <Animated
+                text="Turning Ideas Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
               <p>
                 I&apos;m a software developer based in Campinas BR, specializing
                 in building exceptional websites and mobile applications, and
