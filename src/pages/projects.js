65a69f4ef117a35c5cb472eaa19a3017a2cac9c7
joaoricardoM <joao_pia_mds@hotmail.com>
2023-09-20 17:16:16 -0300

    feat: create project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,165 @@
-import EffectParticles from '@/components/particles'
+import Animated from '@/components/Animated'
+import { GithubIcon } from '@/components/Icons'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import project1 from '../../public/images/projects/screenshot1.jpg'
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full relative flex items-center justify-between rounded-3xl rounded-br-2xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounderd-br-3xl" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-96" />
+      </Link>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl"> {type} </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold ">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark ">{summary}</p>
+        <div className="mt-2 flex items-center ">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounderd-br-3xl" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl"> {type} </span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold ">{title}</h2>
+        </Link>
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="underline text-lg font-semibold"
+          >
+            Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  )
+}
 
 const projects = () => {
   return (
     <>
-      <div className="w-full flex flex-row items-center justify-center">
-        Projetos
-      </div>
+      <Head>
+        <title>João | Projects Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <Animated text="Imagination Trumps Knowledge!" className="mb-16" />
+
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+
+            <div className="col-span-12">
+              {' '}
+              <FeaturedProject
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Netflix Clone"
+                summary="Master Full Stack Netflix Clone development with Next.js 12, React, Tailwind, Prisma, and MongoDB."
+                img={project1}
+                link="/"
+                type="Featured Project"
+                github="/"
+              />
+            </div>
+          </div>
+        </Layout>
+      </main>
     </>
   )
 }
