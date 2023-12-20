2123e06f29c704a48730e54c0c8c746a9efb596e
joaoricardoM <joao_pia_mds@hotmail.com>
2023-12-20 16:59:39 -0300

    fix bugs

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react"
+import { motion } from "framer-motion"
 
 const Skill = ({ name, x, y }) => {
   return (
@@ -40,16 +40,17 @@ const Skills = () => {
           Web
         </motion.div>
 
-        <Skill name="HTML" x="-25vw" y="2vw" />
+        <Skill name="HTML" x="-20vw" y="2vw" />
         <Skill name="CSS" x="-5vw" y="-10vw" />
         <Skill name="JavaScript" x="20vw" y="6vw" />
         <Skill name="ReactJS" x="0vw" y="12vw" />
         <Skill name="NodeJS" x="-20vw" y="-15vw" />
+        <Skill name="GitHub" x="-35vw" y="-5vw" />
         <Skill name="NextJS" x="15vw" y="-12vw" />
         <Skill name="Web Design" x="32vw" y="-5vw" />
         <Skill name="Figma" x="0vw" y="-20vw" />
         <Skill name="MongoDB" x="-25vw" y="18vw" />
-        <Skill name="Tailwind CSS" x="18vw" y="18vw" />
+        <Skill name="Tailwind CSS" x="19vw" y="19vw" />
       </div>
     </>
   )
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
-import Animated from '@/components/Animated'
-import Layout from '@/components/Layout'
-import Particle from '@/components/particles'
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useEffect, useRef } from 'react'
-import profilePic from '../../public/images/profile/profileAbout.jpeg'
-import { useInView, useMotionValue, useSpring } from 'framer-motion'
-import Skills from '@/components/Skills'
-import Experience from '@/components/Experience'
-import Education from '@/components/Education'
-import TransitionEffect from '@/components/TransitionEffect'
+import Animated from "@/components/Animated"
+import Layout from "@/components/Layout"
+import Particle from "@/components/particles"
+import Head from "next/head"
+import Image from "next/image"
+import React, { useEffect, useRef } from "react"
+import profilePic from "../../public/images/profile/profileAbout.jpeg"
+import { useInView, useMotionValue, useSpring } from "framer-motion"
+import Skills from "@/components/Skills"
+import Experience from "@/components/Experience"
+import Education from "@/components/Education"
+import TransitionEffect from "@/components/TransitionEffect"
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null)
@@ -25,7 +25,7 @@ const AnimatedNumbers = ({ value }) => {
   }, [IsInView, value, motionValue])
 
   useEffect(() => {
-    springValue.on('change', (latest) => {
+    springValue.on("change", (latest) => {
       if (ref.current && latest.toFixed(0) <= value) {
         ref.current.textContent = latest.toFixed(0)
       }
@@ -50,7 +50,7 @@ const about = () => {
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <Animated
-            text="Passion fuels Purpuose!"
+            text="Passion fuels Purpose!"
             className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
           />
           <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
@@ -91,11 +91,8 @@ const about = () => {
                 Let&apos;s create something amazing together!
               </p>
             </div>
-            <div
-              className="col-span-2 relative h-max rounded-2xl border-2 border-solid border-dark 
-            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8"
-            >
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+            <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark  bg-light p-8 dark:border-light dark:bg-dark xl:col-span-4 md:col-span-8 md:order-1 ">
+              <div className="absolute top-0 -right-3 -z-10 h-[103%] w-[102%] rounded-[2rem] rounded-br-3xl  bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
                 alt="JoaoCode"
@@ -119,7 +116,7 @@ const about = () => {
                   <AnimatedNumbers value={20} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
-                  Projects Completed{' '}
+                  Projects Completed{" "}
                 </h2>
               </div>
 
