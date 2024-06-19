0190ac953acb96d96207e5d503f8724bcb8db284
joaoricardoM <joao_pia_mds@hotmail.com>
2024-06-19 16:20:11 -0300

    feat: add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,30 @@
-import './globals.css'
-import { Montserrat } from 'next/font/google'
-import NavBar from '@/components/NavBar'
-import Head from 'next/head'
-import Footer from '@/components/Footer'
-import { AnimatePresence } from 'framer-motion'
-import { useRouter } from 'next/router'
+import "./globals.css"
+import { Montserrat } from "next/font/google"
+import NavBar from "@/components/NavBar"
+import Head from "next/head"
+import Footer from "@/components/Footer"
+import { AnimatePresence } from "framer-motion"
+import { useRouter } from "next/router"
+import Script from "next/script"
 
-const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-mont' })
+import * as gtag from "./gtag"
+import { useEffect } from "react"
+
+const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" })
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
@@ -21,6 +36,24 @@ export default function App({ Component, pageProps }) {
       >
         <NavBar />
         <AnimatePresence mode="wait">
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
           <Component key={router.asPath} {...pageProps} />
         </AnimatePresence>
         <Footer />
